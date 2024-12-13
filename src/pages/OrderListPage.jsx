3b4ad1936930d642cc65975ac28e78d22d27bfd0
Bruno Frasan <brunofrancco@gmail.com>
2024-12-13 14:24:01 +0000

    order list router

--- src/pages/OrderListPage.jsx
+++ src/pages/OrderListPage.jsx
@@ -1,40 +1,50 @@
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect, useContext } from 'react'
+import { AuthContext } from '../context/Auth.context'
+import axios from 'axios'
 const OrderListPage = () => {
-    const API_URL = import.meta.env.VITE_APIURL;
+	const { user, isLoggedIn, isAdmin, setOrder, order } =
+		useContext(AuthContext)
+	const [orderDisplayed, setOrderdisplayed] = useState('')
+	const API_URL = import.meta.env.VITE_APIURL
 
-    const [orders, setOrders] = useState();
+	// const [orders, setOrders] = useState()
 
-    useEffect(() => {
-        const fetchOrders = async () => {
-            try {
-                const isToken = localStorage.getItem("authToken");
-                const response = await fetch(`${API_URL}/orders/admin/all-orders`, { headers: { 'Authorization': `Bearer ${isToken}` } });
+	useEffect(() => {
+		const fetchOrders = async () => {
+			try {
+				const isToken = localStorage.getItem('authToken')
+				if (isAdmin) {
+					const response = await fetch(
+						`${API_URL}/orders/admin/all-orders`,
+						{ headers: { Authorization: `Bearer ${isToken}` } }
+					)
+					const { data } = await response.json()
+					console.log(data)
+					setOrderdisplayed(data)
+				} else {
+					const storeOrder = await axios.post(
+						`${API_URL}/user/:userId/make-order`
+					)
+				}
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		fetchOrders()
+	}, [])
 
-                const { data } = await response.json();
-                console.log(data);
+	return (
+		<div>
+			{isLoggedIn ? <h1>Hi {user.name}</h1> : <h1>Please Log in</h1>}
 
-                setOrders(data);
+			<div className='main-container'>
+				{orderDisplayed &&
+					orderDisplayed.map((order) => {
+						return <div key={order._id}>{order}</div>
+					})}
+			</div>
+		</div>
+	)
+}
 
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetchOrders();
-    }, []);
-
-    return (
-        <div className='main-container'>
-            {orders &&
-                orders.map((order, i) => {
-                    return (
-                        <div key={i}>
-                            {order.name}
-                        </div>);
-                })
-            }
-        </div>
-    );
-};
-
-export default OrderListPage;
+export default OrderListPage
