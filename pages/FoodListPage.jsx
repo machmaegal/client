bf3a55a0075ef8d1867c6ecd8dff9a0dfe64e340
Bruno Frasan <brunofrancco@gmail.com>
2024-12-13 09:02:23 +0000

    'friday start'

--- pages/FoodListPage.jsx
+++ pages/FoodListPage.jsx
@@ -1,40 +1,34 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
 const FoodListPage = () => {
-    const API_URL = import.meta.env.VITE_APIURL;
+	const API_URL = import.meta.env.VITE_APIURL
 
-    const [foods, setFoods] = useState();
+	const [foods, setFoods] = useState()
 
-    useEffect(() => {
-        const fetchFoods = async () => {
-            try {
-                const response = await fetch(`${API_URL}/food/dishes`);
+	useEffect(() => {
+		const fetchFoods = async () => {
+			try {
+				const response = await fetch(`${API_URL}/food/dishes`)
 
-                const { data } = await response.json();
-                //console.log(data);
+				const { data } = await response.json()
+				//console.log(data);
 
-                setFoods(data);
+				setFoods(data)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		fetchFoods()
+	}, [])
 
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        fetchFoods();
-    }, []);
+	return (
+		<div className='main-container'>
+			{foods &&
+				foods.map((food, i) => {
+					return <div key={i}>{food.name}</div>
+				})}
+		</div>
+	)
+}
 
-    return (
-        <div className='main-container'>
-            {foods &&
-                foods.map((food, i) => {
-                    return (
-                        <div key={i}>
-                            {food.name}
-                        </div>);
-
-                })
-            }
-        </div>
-    );
-};
-
-export default FoodListPage;
+export default FoodListPage
