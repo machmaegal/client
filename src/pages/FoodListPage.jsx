4cb39ff9261899e023ac5282bd7ce839cb79f563
machmaegal <aka.pleat371@passinbox.com>
2024-12-13 14:10:37 +0100

    camelCase comment

--- src/pages/FoodListPage.jsx
+++ src/pages/FoodListPage.jsx
@@ -1,43 +1,43 @@
-import { useState, useEffect, useContext } from 'react'
-import { AuthContext } from '../context/Auth.context'
+import { useState, useEffect, useContext } from 'react';
+import { AuthContext } from '../context/Auth.context';
 const FoodListPage = () => {
-	const { isLoggedIn, setOrder, order } = useContext(AuthContext)
-	const API_URL = import.meta.env.VITE_APIURL
+	const { isLoggedIn, setOrder, order } = useContext(AuthContext);
+	const API_URL = import.meta.env.VITE_APIURL;
 
 	function handdleAddToCart(item) {
-		setOrder({ ...order, food: [item, ...order.food] })
+		setOrder({ ...order, food: [item, ...order.food] });
 	}
 	// --------------------------
 	// function to convert to camel case
-	String.prototype.camelCase = function () {
-		let newString = ''
+	/* String.prototype.camelCase = function () {
+		let newString = '';
 
 		this.split(' ').forEach(function (str) {
 			newString = `${newString} ${str.substring(0, 1).toUpperCase()}${str
 				.substring(1)
-				.toLowerCase()}` //XD
-		})
+				.toLowerCase()}`;
+		});
 
-		return newString.substring(1)
-	}
+		return newString.substring(1);
+	}; */
 	// ---------------------------------------
-	const [foods, setFoods] = useState()
+	const [foods, setFoods] = useState();
 
 	useEffect(() => {
 		const fetchFoods = async () => {
 			try {
-				const response = await fetch(`${API_URL}/food/dishes`)
+				const response = await fetch(`${API_URL}/food/dishes`);
 
-				const { data } = await response.json()
+				const { data } = await response.json();
 				//console.log(data);
 
-				setFoods(data)
+				setFoods(data);
 			} catch (error) {
-				console.log(error)
+				console.log(error);
 			}
-		}
-		fetchFoods()
-	}, [])
+		};
+		fetchFoods();
+	}, []);
 
 	return (
 		<div className='main-container'>
@@ -45,22 +45,22 @@ const FoodListPage = () => {
 				foods.map((food) => {
 					return (
 						<div key={food._id} className='item-card'>
-							<div>{food.name.camelCase()}</div>
-							<div>{food.description.camelCase()}</div>
-							<div>{food.label[0].camelCase()}</div>
+							<div>{food.name}</div>
+							<div>{food.description}</div>
+							<div>{food.label[0]}</div>
 							<div>{food.price + `€`}</div>
 							<button
 								onClick={() => {
-									handdleAddToCart(food._id)
+									handdleAddToCart(food._id);
 								}}
 							>
 								Add to Cart!
 							</button>
 						</div>
-					)
+					);
 				})}
 		</div>
-	)
-}
+	);
+};
 
-export default FoodListPage
+export default FoodListPage;
