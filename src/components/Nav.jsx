5f6e518856ee229aaed9b065670e7660be538174
Bruno Frasan <brunofrancco@gmail.com>
2024-12-13 13:22:36 +0000

    nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,14 @@
-import { Link, useNavigate } from 'react-router';
+import { Link, useNavigate } from 'react-router'
 
 const Nav = () => {
-    return (
-        <nav>
-            <Link to='/'>food</Link>
-            <Link to='/drinks'>drink</Link>
-            <Link to='/login'>Sign In</Link>
-        </nav>
-    );
-};
+	return (
+		<nav>
+			<Link to='/'>food</Link>
+			<Link to='/drinks'>drink</Link>
+			<Link to='/login'>Sign In</Link>
+			<Link to='/order'>Order</Link>
+		</nav>
+	)
+}
 
-export default Nav;
+export default Nav
