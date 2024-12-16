8e0663d41ceb354feb6f05945f02397389885489
machmaegal <aka.pleat371@passinbox.com>
2024-12-16 02:31:20 +0100

    add responsive css to views with form elements

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,16 @@
-import { Link, useNavigate } from 'react-router'
+import { Link, useNavigate } from 'react-router';
 
 const Nav = () => {
 	return (
 		<nav>
-			<Link to='/'>food</Link>
-			<Link to='/drinks'>drink</Link>
+			<div className='nav-select'>
+				<Link to='/'>food</Link>
+				<Link to='/drinks'>drink</Link>
+				<Link to='/order'>Order</Link>
+			</div>
 			<Link to='/login'>Sign In</Link>
-			<Link to='/order'>Order</Link>
 		</nav>
-	)
-}
+	);
+};
 
-export default Nav
+export default Nav;
