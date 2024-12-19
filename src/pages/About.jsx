89a99db31dcf0f9fa8874d82e227685fd51d7d3f
machmaegal <aka.pleat371@passinbox.com>
2024-12-19 15:10:58 +0100

    fixing deployment

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 const About = () => {
-	return <h1>Two Chefs One Deli</h1>
-}
+	return <h1 id='about'>Two Chefs One Deli</h1>;
+};
 
-export default About
+export default About;
--- src/pages/OrderListPage.jsx
+++ src/pages/OrderListPage.jsx
@@ -1,21 +1,21 @@
-import { useState, useEffect, useContext } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { AuthContext } from '../context/Auth.context'
-import axios from 'axios'
+import { useState, useEffect, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/Auth.context';
+import axios from 'axios';
 const OrderListPage = () => {
 	// const navigate = useNavigate()
 	const { user, isLoggedIn, isAdmin /*setOrder, order */ } =
-		useContext(AuthContext)
+		useContext(AuthContext);
 
-	const [orderDisplayed, setOrderdisplayed] = useState('')
+	const [orderDisplayed, setOrderdisplayed] = useState('');
 	// --------------------------
 	// update order
-	const [updatedOrder, setUpdatedOrder] = useState({})
-	const [total, setTotal] = useState(0)
+	const [updatedOrder, setUpdatedOrder] = useState({});
+	const [total, setTotal] = useState(0);
 
 	// -------------------------------------------
-	const API_URL = import.meta.env.VITE_APIURL
-	const isToken = localStorage.getItem('authToken')
+	const API_URL = import.meta.env.VITE_APIURL;
+	const isToken = localStorage.getItem('authToken');
 
 	// --------------------------
 	// delete order
@@ -23,36 +23,36 @@ const OrderListPage = () => {
 		let deletedOrder = await axios.delete(
 			`${API_URL}/orders/user/${user._id}/user-order/${id}`,
 			{ headers: { Authorization: `Bearer ${isToken}` } }
-		)
+		);
 		let orders = orderDisplayed.filter((order) => {
 			if (order._id === id) {
 				// console.log(order._id, id)
 			} else {
-				return order
+				return order;
 			}
-		})
-		setOrderdisplayed(orders)
+		});
+		setOrderdisplayed(orders);
 	}
 	// ---------------------
 	// user getting one order after click
-	async function getOneOrder(id) {
-		const data = await axios.get(
-			`${API_URL}/orders/user/${user._id}/user-order/${id}`,
-			{ headers: { Authorization: `Bearer ${isToken}` } }
-		)
-		console.log(data.data.data)
-	}
-	// ---------------------
-	// admin getting one order after click
-	async function adminGetOneOrder(id) {
-		const data = await axios.get(
-			`${API_URL}/orders/admin/user-order/${id}`,
-			{
-				headers: { Authorization: `Bearer ${isToken}` },
-			}
-		)
-		console.log(data.data.data)
-	}
+	/* 	async function getOneOrder(id) {
+			const data = await axios.get(
+				`${API_URL}/orders/user/${user._id}/user-order/${id}`,
+				{ headers: { Authorization: `Bearer ${isToken}` } }
+			);
+			console.log(data.data.data);
+		}
+		// ---------------------
+		// admin getting one order after click
+		async function adminGetOneOrder(id) {
+			const data = await axios.get(
+				`${API_URL}/orders/admin/user-order/${id}`,
+				{
+					headers: { Authorization: `Bearer ${isToken}` },
+				}
+			);
+			console.log(data.data.data);
+		} */
 	// ------------------------
 	useEffect(() => {
 		const fetchOrders = async () => {
@@ -61,30 +61,30 @@ const OrderListPage = () => {
 					const response = await fetch(
 						`${API_URL}/orders/admin/all-orders`,
 						{ headers: { Authorization: `Bearer ${isToken}` } }
-					)
-					const { data } = await response.json()
-					// console.log(data)
-					return setOrderdisplayed(data)
+					);
+					const { data } = await response.json();
+					//console.log(data);
+					return setOrderdisplayed(data);
 				} /*if (order.drink.length > 0 || order.food.length > 0) */ else {
 					const data = await axios.get(
 						`${API_URL}/orders/user/${user._id}/user-orders`,
 						{ headers: { Authorization: `Bearer ${isToken}` } }
-					)
-					setOrderdisplayed(data.data.data)
+					);
+					setOrderdisplayed(data.data.data);
 				}
 			} catch (error) {
-				console.log(error)
+				console.log(error);
 			}
-		}
-		fetchOrders()
-	}, [user._id, updatedOrder._id])
+		};
+		fetchOrders();
+	}, [user._id, updatedOrder._id]);
 	// -----------------------------
 	// total function
 	function totalBill(order) {
-		let total = 0
-		order.food.forEach((food) => (total += food.price))
-		order.drink.forEach((drink) => (total += drink.price))
-		return total
+		let total = 0;
+		order.food.forEach((food) => (total += food.price));
+		order.drink.forEach((drink) => (total += drink.price));
+		return total;
 	}
 	// ----------------------------
 	return (
@@ -100,7 +100,7 @@ const OrderListPage = () => {
 								className='list-item'
 								key={order._id}
 								onMouseEnter={() => {
-									setUpdatedOrder(order)
+									setUpdatedOrder(order);
 								}}
 							>
 								<p className='name-and-price-tack'>order id:</p>
@@ -127,7 +127,7 @@ const OrderListPage = () => {
 												Remove item
 											</button> */}
 										</div>
-									)
+									);
 								})}
 								{order.drink.map((drink, i) => {
 									return (
@@ -144,7 +144,7 @@ const OrderListPage = () => {
 												Remove item
 											</button> */}
 										</div>
-									)
+									);
 								})}
 								<br />
 								<div className='name-price-format-container'>
@@ -157,17 +157,17 @@ const OrderListPage = () => {
 								<button
 									className='submit-button'
 									onClick={() => {
-										hadleDelete(order._id)
+										hadleDelete(order._id);
 									}}
 								>
 									Delete Order
 								</button>
 							</div>
-						)
+						);
 					})}
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default OrderListPage
+export default OrderListPage;
